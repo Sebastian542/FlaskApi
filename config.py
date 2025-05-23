c53d6304285e3c2d24d842b2a7ac37dee051bf9b
Juan Sebastian Muñoz Peñuela <84586099+Sebastian542@users.noreply.github.com>
2025-05-23 13:45:10 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
 MONGO_URI = "mongodb+srv://Hoyos:<db_password>@ferreterialogs.i9tps73.mongodb.net/?retryWrites=true&w=majority&appName=FerreteriaLogs"
 
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = "postgresql://root:SVtoDZA0bt6Zuf3FF56Lfr6bFQsqdI74@dpg-d0obb9uuk2gs73ftusdg-a.db.render.com/ferreteria_mejorada"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
