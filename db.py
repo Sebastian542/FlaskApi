8bd490fb992499f58ddcf1beac7049c4653dd120
Juan Sebastian Muñoz Peñuela <84586099+Sebastian542@users.noreply.github.com>
2025-05-23 17:11:26 -0500

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,15 @@
 import psycopg2
-from psycopg2.extras import RealDictCursor
 
-DATABASE_URL = "postgresql://root:SVtoDZA0bt6Zuf3FF56Lfr6bFQsqdI74@dpg-d0obb9uuk2gs73ftusdg-a.oregon-postgres.render.com/ferreteria_mejorada"
-
-def get_connection():
-    return psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
+def obtener_conexion():
+    try:
+        conn = psycopg2.connect(
+            host="dpg-d0obb9uuk2gs73ftusdg-a.oregon-postgres.render.com",
+            port=5432,
+            database="ferreteria_mejorada",
+            user="root",
+            password="SVtoDZA0bt6Zuf3FF56Lfr6bFQsqdI74"
+        )
+        return conn
+    except Exception as e:
+        print("❌ Error al conectar con psycopg2:", e)
+        return None
