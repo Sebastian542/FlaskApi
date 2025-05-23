4cdf950450676bb3d37410a566c7656c23cb7dfd
Juan Sebastian Muñoz Peñuela <84586099+Sebastian542@users.noreply.github.com>
2025-05-23 15:54:15 -0500

    Update db.py

--- db.py
+++ db.py
@@ -1,38 +1,7 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
 
-DB_CONFIG = {
-    "host": "dpg-d0obb9uuk2gs73ftusdg-a.db.render.com",
-    "port": 5432,
-    "dbname": "ferreteria_mejorada",
-    "user": "root",
-    "password": "SVtoDZA0bt6Zuf3FF56Lfr6bFQsqdI74"
-}
+DATABASE_URL = "postgresql://root:SVtoDZA0bt6Zuf3FF56Lfr6bFQsqdI74@dpg-d0obb9uuk2gs73ftusdg-a.oregon-postgres.render.com/ferreteria_mejorada"
 
 def get_connection():
-    conn = psycopg2.connect(
-        host=DB_CONFIG["host"],
-        port=DB_CONFIG["port"],
-        dbname=DB_CONFIG["dbname"],
-        user=DB_CONFIG["user"],
-        password=DB_CONFIG["password"],
-        cursor_factory=RealDictCursor
-    )
-    return conn
-
-def init_db():
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("""
-    CREATE TABLE IF NOT EXISTS producto (
-        id SERIAL PRIMARY KEY,
-        nombre VARCHAR(100) NOT NULL,
-        descripcion TEXT,
-        stock INTEGER NOT NULL,
-        precio NUMERIC(10, 2) NOT NULL,
-        id_categoria INTEGER
-    );
-    """)
-    conn.commit()
-    cursor.close()
-    conn.close()
+    return psycopg2.connect(DATABASE_URL, cursor_factory=RealDictCursor)
